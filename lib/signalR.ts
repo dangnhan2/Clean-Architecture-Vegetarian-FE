09dd2a2e7ca603f368381a19d9d99675bff91e2d
nhan <nhanbadao992@gmail.com>
2026-02-21 23:57:53 +0700

    Fix - Description type in production detail page

--- lib/signalR.ts
+++ lib/signalR.ts
@@ -2,7 +2,7 @@ import * as signalR from "@microsoft/signalr";
 
 export const createSignalRConnection = (accessToken?: string) => {
     return new signalR.HubConnectionBuilder()
-         .withUrl("https://localhost:8081/hubs/notification", {
+         .withUrl(`${process.env.NEXT_PUBLIC_SIGNALR_HUB_URI}`, {
             accessTokenFactory: () => accessToken || localStorage.getItem("access_token") || "",
             withCredentials : true,          
          })
